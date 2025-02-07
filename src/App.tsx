af5ced20bfe6ad83d10222a91567fe26be97a3c2
Sojo C Johny <sojocjohny37@gmail.com>
2025-02-07 14:29:04 +0530

    Added a simple navbar and a settings page with color theme selection.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
+import Settings from './components/Settings'
+import { ThemeProvider } from './context/ThemeContext'
 
 function App() {
   return (
-    <h1>Master management</h1>
+    <ThemeProvider>
+      <Router>
+        <div className="app-container">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/" element={<h1>Master Management</h1>} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </ThemeProvider>
   )
 }
 
