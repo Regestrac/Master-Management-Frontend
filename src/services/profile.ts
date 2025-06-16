9460fd723335513bb5e2a3f3526cf169008d5937
Sojo C Johny <sojocjohny37@gmail.com>
2025-06-16 12:26:36 +0530

    Added form and integrated api to update profile.

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,5 +1,11 @@
-import { getHandler } from 'services/api';
+import { getHandler, postHandler } from 'services/api';
 
 export const getProfile = () => getHandler({
   path: 'profile',
+});
+
+export const updateProfile = (data: object) => postHandler({
+  path: 'profile',
+  body: JSON.stringify(data),
+  method: 'PUT',
 });
